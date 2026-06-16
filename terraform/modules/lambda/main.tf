294583f18652b20955260e582ae40a31b4589006
an4she21 <anas.hennour.10@gmail.com>
2026-06-16 02:37:34 +0100

    refactor: rename folders for clarity

--- terraform/modules/lambda/main.tf
+++
@@ -1,8 +0,0 @@
-# DEPRECATED: Lambda has been replaced by ECS Fargate.
-# This module is intentionally empty.
-# The AI agent now runs as an ECS Fargate service.
-# See: terraform/modules/ecs/main.tf
-#
-# To destroy the old Lambda resources manually:
-#   terraform destroy -target=module.lambda_ai
-# Then remove this module block from live/dev/main.tf
